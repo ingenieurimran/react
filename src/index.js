fd56c4557a621b53c7dfb75688948142218295d4
Imran Ali <engr.imran_1242@hotmail.com>
2022-08-06 21:52:31 +0200

    First commit introduction to JSX and Babel

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1> I Love To Learn</h1>
+    <ul>
+      <li>Node.js</li>
+      <li>React</li>
+      <li>mongodb and Mongoose</li>
+    </ul>
+  </div>
+  , document.getElementById("root"));
+
