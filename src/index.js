e0b2140bcf2481ce74f4bdba8b43dda82204ce25
Imran Ali <engr.imran_1242@hotmail.com>
2022-08-07 22:53:30 +0200

    JSX Attribute and Syling React Elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 const react = 'React';
 const fullName = 'Imran Ali';
 const currentYear = new Date().getFullYear();
-ReactDOM.render(
+
+const imgg = 'https://picsum.photos/200'
+root.render(
   <div>
-    <h1> Hello {`${react}`}</h1>
+    <h1 className='heading' contentEditable='true' spellCheck='false'> Hello {`${react}`}</h1>
     <p>Created by {`${fullName}`}</p>
     <p>Copyright {`${currentYear}`}</p>
+    <div>
+      <img src={imgg} />
+    </div>
   </div>
-  , document.getElementById("root"));
+);
 
 
 
