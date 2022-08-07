03cb779d3a747b3bb00b4df50cad62d3e58f218d
Imran Ali <engr.imran_1242@hotmail.com>
2022-08-08 00:26:12 +0200

    React Styling Practice

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
+
+const container = document.getElementById('root');
 const root = createRoot(container);
+const hour = new Date(2022, 1, 1, 19).getHours();
 
-const react = 'React';
-const fullName = 'Imran Ali';
-const currentYear = new Date().getFullYear();
+const customStyle = {
+  color: '',
+};
+let greeting;
+let colorName;
+if (hour < 12 && hour >= 0) {
+  greeting = 'Good Morning';
+  customStyle.color = 'red';
+} else if (hour < 18 && hour >= 12) {
+  greeting = 'Good Afternoon';
+  customStyle.color = 'green';
+} else {
+  greeting = 'Good Evening';
+  customStyle.color = 'blue';
+}
 
-const imgg = 'https://picsum.photos/200'
-root.render(
-  <div>
-    <h1 className='heading' contentEditable='true' spellCheck='false'> Hello {`${react}`}</h1>
-    <p>Created by {`${fullName}`}</p>
-    <p>Copyright {`${currentYear}`}</p>
-    <div>
-      <img src={imgg} />
-    </div>
-  </div>
-);
-
-
-
-
+root.render(<h1 className='heading' style={customStyle}>{greeting}</h1>)
