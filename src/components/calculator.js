7994f4de140ea7172ce4259fbd3b52f7755d8020
Imran Ali <engr.imran_1242@hotmail.com>
2022-08-09 23:24:27 +0200

    ES6 Import, Export and Modules

---
+++ src/components/calculator.js
@@ -0,0 +1,18 @@
+function Add(x1, x2) {
+    return (x1 + x2)
+}
+
+function Multiply(x1, x2) {
+    return (x1 * x2)
+}
+
+function Substract(x1, x2) {
+    return (x1 - x2)
+}
+
+function Divide(x1, x2) {
+    return (x1 / x2)
+}
+
+// export default Add
+export { Add, Multiply, Substract, Divide }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./components/App"
 
+// import Add, { Multiply } from "./components/calculator.js"
+import * as cal from "./components/calculator.js"
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <App />
+  <ul>
+    <li>{cal.Add(1, 2)}</li>
+    <li>{cal.Multiply(2, 3)}</li>
+    <li>{cal.Substract(7, 2)}</li>
+    <li>{cal.Divide(5, 2)}</li>
+  </ul>
 )
